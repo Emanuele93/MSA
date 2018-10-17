213012cc7b4f89a7f35f242f29a8c0563015bf49
Emanuele <emanuele.lattanzio@mail.polimi.it>
2018-10-17 12:07:19 +0200

    pruning, 2 Pre e 1 Post

--- prova_albero/prova.py
+++ prova_albero/prova.py
@@ -1,11 +1,59 @@
 class Tree:
-    def __init__(self, col, value, true_branch_label, false_branch_label, true_branch=None, false_branch=None):
+    def __init__(self, col, value, label, true_branch, false_branch,
+                 true_branch_number, false_branch_number, gini_value, gain):
         self.col = col
         self.value = value
-        self.true_branch_label = true_branch_label
-        self.false_branch_label = false_branch_label
+        self.label = label
         self.true_branch = true_branch
         self.false_branch = false_branch
+        self.true_branch_number = true_branch_number
+        self.false_branch_number = false_branch_number
+        self.gini_value = round(gini_value, 2)
+        self.gain = gain
+
+    def __str__(self, level=0):
+        if self.col is None:
+            ret = "\t" * level + "[ True: " + str(self.true_branch_number) + ", False: " \
+                  + str(self.false_branch_number) + ", Gini: " + str(self.gini_value) + " ]\n"
+        else:
+            ret = "\t" * level + "[ X" + str(self.col) + " <= " + str(self.value) + " --> True: " \
+                  + str(self.true_branch_number) + ", False: " + str(self.false_branch_number) \
+                  + ", Gini: " + str(self.gini_value) + " ]\n"
+        if isinstance(self.true_branch, Tree):
+            ret += self.true_branch.__str__(level + 1)
+        if isinstance(self.false_branch, Tree):
+            ret += self.false_branch.__str__(level + 1)
+        return ret
+
+    def export_graphviz(self, number=0):
+        ret = ""
+        if number == 0:
+            ret += "digraph Tree {\nnode [shape=box, style=\"rounded\", color=\"black\", " \
+                   "fontname=helvetica] ;\nedge [fontname=helvetica] ;" + "\n"
+        if self.true_branch is None and self.false_branch is None:
+            ret += str(number) + "[label=<gini = " + str(self.gini_value) + "<br/>samples = " \
+                   + str(self.false_branch_number + self.true_branch_number) + ">] ;" + '\n'
+        else:
+            ret += str(number) + " [label=<X<SUB>" + str(self.col) + "</SUB> &le; " + str(self.value) \
+                   + "<br/>gini = " + str(self.gini_value) + "<br/>samples = " \
+                   + str(self.false_branch_number + self.true_branch_number) + ">] ;" + '\n'
+        n_f = n = number + 1
+        if self.true_branch is not None:
+            stringa, n_f = self.true_branch.export_graphviz(number=n_f)
+            ret += stringa + str(number) + " -> " + str(number + 1)
+            if number == 0:
+                ret += "[labeldistance=2.5, labelangle=45, headlabel=\"True\"]"
+            ret += " ;" + '\n'
+            n = n_f
+        if self.false_branch is not None:
+            stringa, n = self.false_branch.export_graphviz(number=n_f)
+            ret += stringa + str(number) + " -> " + str(n_f)
+            if number == 0:
+                ret += "[labeldistance=2.5, labelangle=-45, headlabel=\"False\"]"
+            ret += " ;" + '\n'
+        if number == 0:
+            ret += "}"
+        return str(ret), n + 1
 
 
 def unique_counts(labels):
@@ -32,30 +80,37 @@ def gini(labels):
     return imp
 
 
-def splittable(data, labels):
-    # check_labels = len(data) > 1
-    # 4 righe sotto per non dividere i data con etichette tutte uguali, riga sopra per arrivare ai singoli nodi
-
-    check_labels = False
+def pre_pruning(labels):
     for i in labels:
         if i != labels[0]:
-            check_labels = True
+            return True
+    return False
 
-    if check_labels:
-        best_col = 0
+
+def splittable(data, labels, parent_gini, pre_pruning_no_useless_split, pre_pruning_minimum_n_object):
+    if len(data) > 1 and (not pre_pruning_no_useless_split or pre_pruning(labels)):
+        best_col = -1
         best_value = 0
-        best_col_gini = 1
+        best_gain = -1
+        true_best_gini = 1
+        false_best_gini = 1
         for i in range(0, len(data[0])):
             for j in data:
                 t_b, f_b, t_b_l, f_b_l = split(data, labels, i, j[i])
-                if len(t_b) != 0 and len(f_b) != 0:
-                    gini_value = min(gini(t_b_l), gini(f_b_l))
-                    if gini_value < best_col_gini:
+                if len(t_b) >= pre_pruning_minimum_n_object and len(f_b) >= pre_pruning_minimum_n_object:
+                    gini_split_1 = gini(t_b_l)
+                    gini_split_2 = gini(f_b_l)
+                    p = float(len(t_b_l)) / len(t_b_l + f_b_l)
+                    gain = parent_gini - p * gini_split_1 - (1 - p) * gini_split_2
+                    if gain > best_gain:
                         best_col = i
                         best_value = j[i]
-                        best_col_gini = gini_value
-        return best_col, best_value
-    return -1, 0
+                        true_best_gini = gini_split_1
+                        false_best_gini = gini_split_2
+                        best_gain = gain
+        if best_col >= 0:
+            return best_col, best_value, true_best_gini, false_best_gini, best_gain
+    return -1, 0, 0, 0, 0
 
 
 def major_label(labels):
@@ -78,7 +133,7 @@ def split(data, labels, col, value):
     true_branch_labels = []
     false_branch_labels = []
     for i in range(0, len(data)):
-        if data[i][col] > value:
+        if data[i][col] <= value:
             true_branch.append(data[i])
             true_branch_labels.append(labels[i])
         else:
@@ -87,26 +142,33 @@ def split(data, labels, col, value):
     return true_branch, false_branch, true_branch_labels, false_branch_labels
 
 
-def fit(data, labels):
-    col, value = splittable(data, labels)
+def fit(data, labels, gini_value, pre_pruning_no_useless_split=True, pre_pruning_minimum_n_object=1,
+        post_pruning_pessimistic = True, post_pruning_reduced_error = True):
+    col, value, gini_value_true, gini_value_false, gain = \
+        splittable(data, labels, gini_value, pre_pruning_no_useless_split, pre_pruning_minimum_n_object)
     if col >= 0:
         true_branch, false_branch, true_branch_labels, false_branch_labels = split(data, labels, col, value)
-        true_branch_label = major_label(true_branch_labels)
-        false_branch_label = major_label(false_branch_labels)
-        false_branch = fit(false_branch, false_branch_labels)
-        true_branch = fit(true_branch, true_branch_labels)
-        return Tree(col, value, true_branch_label, false_branch_label, true_branch, false_branch)
-    return data
+        false_branch = fit(false_branch, false_branch_labels, gini_value_false,
+                           pre_pruning_no_useless_split=pre_pruning_no_useless_split,
+                           pre_pruning_minimum_n_object=pre_pruning_minimum_n_object)
+        true_branch = fit(true_branch, true_branch_labels, gini_value_true,
+                           pre_pruning_no_useless_split=pre_pruning_no_useless_split,
+                           pre_pruning_minimum_n_object=pre_pruning_minimum_n_object)
+        return Tree(col, value, major_label(labels), true_branch, false_branch,
+                    len(true_branch_labels), len(false_branch_labels), gini_value, gain)
+    return Tree(None, None, major_label(labels), None, None, len(data), 0, 0, 0)
 
 
 def classifier(data, node):
-    if data[node.col] > node.value:
-        if isinstance(node.true_branch, Tree):
+    if node.col is None:
+        return node.label
+    if data[node.col] <= node.value:
+        if node.true_branch.col is not None:
             return classifier(data, node.true_branch)
-        return node.true_branch_label
-    if isinstance(node.false_branch, Tree):
+        return node.true_branch.label
+    if node.false_branch.col is not None:
         return classifier(data, node.false_branch)
-    return node.false_branch_label
+    return node.false_branch.label
 
 
 def predict(data, root):
@@ -114,3 +176,107 @@ def predict(data, root):
     for element in data:
         ris.append(classifier(element, root))
     return ris
+
+
+def post_pruning(tree, min_gain):
+    if tree.true_branch.col is not None:
+        post_pruning(tree.true_branch, min_gain)
+    if tree.false_branch.col is not None:
+        post_pruning(tree.false_branch, min_gain)
+
+    if tree.true_branch.col is None and tree.false_branch.col is None:
+        if tree.gain < min_gain:
+            tree.col = None
+            tree.value = None
+            tree.true_branch = None
+            tree.false_branch = None
+
+
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.tree import DecisionTreeClassifier
+import graphviz
+from sklearn import tree
+import os
+
+os.environ["PATH"] += os.pathsep + 'C:/Program Files (x86)/Graphviz2.38/bin/'
+
+
+data_X = [[1., 1.], [2., 2.], [3., 3.], [4., 4.], [5., 5.], [6., 6.], [7., 7.], [8., 8.], [9., 9.], [10., 10.], [11., 11.], [12., 12.]]
+data_Y = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11"]
+
+n_classes = len(unique_counts(data_Y))
+plot_step = 0.02
+
+data = {"data": [], "target": [], "target_names": [], "DESCR": "no descr", "feature_names": [], "filename": "no name"}
+for el in data_X:
+    data["data"].append(el)
+for el in data_Y:
+    data["target"].append(int(el))
+for el in unique_counts(data_Y):
+    data["target_names"].append(el)
+for el in range(0, len(data_X[0])):
+    data["feature_names"].append("X" + str(el))
+data["data"] = np.array(data["data"])
+data["target"] = np.array(data["target"])
+data["target_names"] = np.array(data["target_names"])
+
+combinations = []
+for i in range(0, len(data_X[0]) - 1):
+    for j in range(i + 1, len(data_X[0])):
+        combinations.append([i, j])
+
+for pairidx, pair in enumerate(combinations):
+    # We only take the two corresponding features
+    X = data["data"][:, pair]
+    y = data["target"]
+
+    # Train
+    clf = DecisionTreeClassifier().fit(X, y)
+
+    dot_data = tree.export_graphviz(clf, rounded=True, special_characters=True)
+
+    s = dot_data.split("<br/>value = [")
+    for i in range(1, len(s)):
+        while s[i][0] != ']':
+            s[i] = s[i][1:]
+        s[0] += s[i][1:]
+    dot_data = s[0]
+
+    graph = graphviz.Source(dot_data)
+    graph.render("Scikit_tree")
+
+    # Plot the decision boundary
+    n = np.sqrt(len(combinations))
+    if n % 1 > 0:
+        n = n + 1
+    n = int(n)
+    if n * (n - 1) == len(combinations):
+        plt.subplot(n - 1, n, pairidx + 1)
+    else:
+        plt.subplot(n, n, pairidx + 1)
+
+    x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, plot_step),
+                         np.arange(y_min, y_max, plot_step))
+    plt.tight_layout(h_pad=0.5, w_pad=0.5, pad=2.5)
+
+    Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+    cs = plt.contourf(xx, yy, Z, cmap=plt.cm.RdYlBu)
+
+    plt.xlabel(data["feature_names"][pair[0]])
+    plt.ylabel(data["feature_names"][pair[1]])
+
+    # Plot the training points
+    for i in range(0, n_classes):
+        idx = np.where(y == i)
+        plt.scatter(X[idx, 0], X[idx, 1], c="w", label=data["target_names"][i],
+                    cmap=plt.cm.RdYlBu, edgecolor='black', s=15)
+
+plt.suptitle("Decision surface of a decision tree using paired features")
+plt.axis("tight")
+plt.show()
+"""
--- prova_scikit-learn/prova.py
+++ prova_scikit-learn/prova.py
@@ -1,12 +1,20 @@
+import graphviz
+import prova_albero.prova as our_decision_tree_classifier
 from sklearn import tree
 from random import randint, shuffle
+import os
 
-import prova_albero.prova as our_decision_tree_classifier
+os.environ["PATH"] += os.pathsep + 'C:/Program Files (x86)/Graphviz2.38/bin/'
 
 data_set_name = "tic-tac-toe"  # krkopt, poker-hand-testing, poker-hand-training-true, tic-tac-toe
 data_root = "../data_set/" + data_set_name + ".data"
-test_set_relation = 1 / 3
-cross_validation_number = 3
+test_set_relation = 3 / 10
+cross_validation_number = 5  # di solito 5 o 10
+pre_pruning_minimum_n_object = 1
+pre_pruning_no_useless_split = True
+our_tree = True
+scikit_tree = True
+print_tree = True
 
 file = open(data_root, "r")
 data = file.readlines()
@@ -41,14 +49,15 @@ elif data_set_name == "poker-hand-training-true":
         while q < len(d_elements) - 2:
             w = q + 2
             while w < len(d_elements):
-                if d_elements[q] > d_elements[w] or (d_elements[q] == d_elements[w] and d_elements[q - 1] > d_elements[w - 1]):
+                if d_elements[q] > d_elements[w] or (
+                        d_elements[q] == d_elements[w] and d_elements[q - 1] > d_elements[w - 1]):
                     temp = d_elements[q]
                     d_elements[q] = d_elements[w]
                     d_elements[w] = temp
                     temp = d_elements[q - 1]
                     d_elements[q - 1] = d_elements[w - 1]
                     d_elements[w - 1] = temp
-                w = w+2
+                w = w + 2
             q = q + 2
         string = str(d_elements[0])
         for e in range(1, len(d_elements)):
@@ -56,7 +65,6 @@ elif data_set_name == "poker-hand-training-true":
         string = string + "," + label
         new_data.append(string)
     data = new_data.copy()
-
 
 training_set = []
 test_set = []
@@ -68,7 +76,7 @@ def resubstitution_method():
     training_set = data.copy()
     test_set = data.copy()
 
-    resolution()
+    resolution("resubstitution")
 
 
 def test_set_method():
@@ -83,7 +91,7 @@ def test_set_method():
         test_set.append(training_set[n])
         training_set.pop(n)
 
-    resolution()
+    resolution("test_set")
 
 
 def cross_validation_method():
@@ -99,18 +107,53 @@ def cross_validation_method():
     for j in range(0, len(training_set)):
         group_of_test_set[j % cross_validation_number].append(training_set[j])
 
-    results = []
+    scikit_results = []
+    our_results = []
+    i = 1
     for j in group_of_test_set:
         test_set = j
         training_set = data.copy()
         for ts in test_set:
             training_set.remove(ts)
-        results.append(float(resolution()))
 
-    print("Cross validation evaluation: " + str(int(sum(results) / len(results) * 100)) + "%\n")
+        if scikit_tree and our_tree:
+            s_r, o_r = resolution("cross_validation_" + str(i))
+            scikit_results.append(float(s_r))
+            our_results.append(float(o_r))
+        elif scikit_tree:
+            s_r = resolution("cross_validation_" + str(i))
+            scikit_results.append(float(s_r))
+        elif our_tree:
+            o_r = resolution("cross_validation_" + str(i))
+            our_results.append(float(o_r))
+        i += 1
+
+    if scikit_tree:
+        print("Scikit cross validation evaluation: " + str(int(sum(scikit_results) / len(scikit_results) * 100)) + "%")
+        if not our_tree:
+            print("")
+    if our_tree:
+        print("Our cross validation evaluation: " + str(int(sum(our_results) / len(our_results) * 100)) + "%\n")
 
 
-def resolution():
+def print_tree(decision_tree, name):
+    if print_tree:
+        tree_string, a = decision_tree.export_graphviz()
+        graph = graphviz.Source(tree_string)
+        graph.render(name)
+
+
+def print_precision(ris, test_y, text):
+    correct_evaluation = 0
+    for k in range(0, len(ris)):
+        if str(ris[k]) == str(test_y[k]):
+            correct_evaluation = correct_evaluation + 1
+    print(text + str(correct_evaluation) + " / " + str(len(test_y)) + " = " +
+          str(int(int(correct_evaluation) / len(test_y) * 100)) + "%")
+    return correct_evaluation
+
+
+def resolution(evaluation_name):
     global i
 
     print("Training_set len: " + str(len(training_set)))
@@ -135,30 +178,63 @@ def resolution():
         elements = [int(j) for j in elements]
         test_X.append(elements.copy())
 
-    """
-    clf = tree.DecisionTreeClassifier()
-    clf = clf.fit(training_X, training_Y)
-    ris = clf.predict(test_X)
-    """
-    clf = our_decision_tree_classifier
-    decision_tree = clf.fit(training_X, training_Y)
-    ris = clf.predict(test_X, decision_tree)
+    scikit_correct_evaluation = 0
+    our_correct_evaluation = 0
 
-    correct_evaluation = 0
-    for i in range(0, len(ris)):
-        if str(ris[i]) == str(test_Y[i]):
-            correct_evaluation = correct_evaluation + 1
-    print("Correct evaluation: " + str(correct_evaluation) + " / " + str(len(test_Y)) +
-          " = " + str(int(int(correct_evaluation) / len(test_Y) * 100)) + "%\n")
+    if scikit_tree:
+        clf = tree.DecisionTreeClassifier()
+        clf = clf.fit(training_X, training_Y)
+        Scikit_ris = clf.predict(test_X)
+        if print_tree:
+            dot_data = tree.export_graphviz(clf, rounded=True, special_characters=True)
+            s = dot_data.split("<br/>value = [")
+            for k in range(1, len(s)):
+                while s[k][0] != ']':
+                    s[k] = s[k][1:]
+                s[0] += s[k][1:]
+            dot_data = s[0]
+            graph = graphviz.Source(dot_data)
+            graph.render("Scikit_tree_" + evaluation_name)
+        scikit_correct_evaluation = print_precision(Scikit_ris, test_Y, "Scikit correct evaluation: ")
 
-    return int(correct_evaluation) / len(test_Y)
+    if our_tree:
+        clf = our_decision_tree_classifier
+        decision_tree = clf.fit(training_X, training_Y, clf.gini(training_Y),
+                                pre_pruning_no_useless_split=pre_pruning_no_useless_split,
+                                pre_pruning_minimum_n_object=pre_pruning_minimum_n_object)
+        Our_ris = clf.predict(test_X, decision_tree)
+        print_tree(decision_tree, "Our_tree_" + evaluation_name)
+        our_correct_evaluation = print_precision(Our_ris, test_Y, "Our correct evaluation: ")
+
+        """
+        decision_tree_pruning = decision_tree
+        our_correct_evaluation = 0  # TODO da togliere questa riga
+        clf.post_pruning(decision_tree_pruning, 0.4)
+        Our_ris = clf.predict(test_X, decision_tree_pruning)
+        if print_tree:
+            tree_string, a = decision_tree_pruning.export_graphviz()
+            graph = graphviz.Source(tree_string)
+            graph.render("Our_tree_with_post_pruning" + evaluation_name)
+        for i in range(0, len(Our_ris)):
+            if str(Our_ris[i]) == str(test_Y[i]):
+                our_correct_evaluation = our_correct_evaluation + 1
+        print("Our correct evaluation with post pruning: " + str(our_correct_evaluation) + " / " + str(len(test_Y)) +
+              " = " + str(int(int(our_correct_evaluation) / len(test_Y) * 100)) + "%")
+        """
+    print("")
+    if our_tree and scikit_tree:
+        return int(scikit_correct_evaluation) / len(test_Y), int(our_correct_evaluation) / len(test_Y)
+    if scikit_tree:
+        return int(scikit_correct_evaluation) / len(test_Y)
+    if our_tree:
+        return int(our_correct_evaluation) / len(test_Y)
 
 
-print("\nMetodo di valutazione dell'errore: risostituzione")
-resubstitution_method()
+# print("\nMetodo di valutazione dell'errore: risostituzione")
+# resubstitution_method()
 
 print("\nMetodo di valutazione dell'errore: test set")
 test_set_method()
 
-print("\nMetodo di valutazione dell'errore: cross validation")
-cross_validation_method()
+# print("\nMetodo di valutazione dell'errore: cross validation")
+# cross_validation_method()
