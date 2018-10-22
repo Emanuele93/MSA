909081e3592cec6d362c0b1e3165be1ba5cec107
Emanuele <emanuele.lattanzio@mail.polimi.it>
2018-10-22 12:02:51 +0200

    minor fix

--- prova_scikit-learn/codice.py
+++ prova_scikit-learn/codice.py
@@ -6,7 +6,7 @@ import os
 
 os.environ["PATH"] += os.pathsep + 'C:/Program Files (x86)/Graphviz2.38/bin/'
 
-data_set_name = "tic-tac-toe"  # krkopt, poker-hand-testing, poker-hand-training-true, tic-tac-toe
+data_set_name = "poker-hand-training-true"  # krkopt, poker-hand-testing, poker-hand-training-true, tic-tac-toe
 data_root = "../data_set/" + data_set_name + ".data"
 our_tree = True
 scikit_tree = True
@@ -244,6 +244,23 @@ def resolution(evaluation_name):
                 os.remove("trees/Scikit_tree_" + evaluation_name)
 
     if post_pruning_reduced_error:
+        clf = our_decision_tree_classifier
+        decision_tree, num = clf.fit(training_X, training_Y, clf.gini(training_Y),
+                                     pre_pruning_no_useless_split=pre_pruning_no_useless_split,
+                                     pre_pruning_minimum_n_object=1)
+        Our_ris = clf.predict(test_X, decision_tree)
+        our_correct_evaluation = print_precision(Our_ris, test_Y, "Our correct evaluation: ", num=num)
+
+        if best_our_correct_evaluation < our_correct_evaluation \
+                or ((best_our_correct_evaluation_number > num or best_our_correct_evaluation_number == -1)
+                    and best_our_correct_evaluation <= our_correct_evaluation):
+            best_our_correct_evaluation = our_correct_evaluation
+            best_our_correct_evaluation_number = num
+            draw_tree(decision_tree, " (" + str(our_correct_evaluation) + "-" + str(len(test_Y)) + " "
+                      + str(num) + "n) Our_tree_" + evaluation_name)
+        if local_best_our_evaluation < our_correct_evaluation:
+            local_best_our_evaluation = our_correct_evaluation
+
         num_of_validation = len(data) * 1 / 10
         while len(validation_set_X) < num_of_validation:
             validation_set_X.append(training_X[0])
@@ -261,6 +278,8 @@ def resolution(evaluation_name):
                                              pre_pruning_no_useless_split=pre_pruning_no_useless_split,
                                              pre_pruning_minimum_n_object=i)
                 str_name = ""
+                if post_pruning_reduced_error:
+                    str_name = " with_growing_set"
                 if i > 1:
                     str_name += " with_pre_cut_at_" + str(i) + "_elements"
                 if j > 0:
